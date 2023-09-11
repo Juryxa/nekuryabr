9ff8f2d8c12d3d59adffd2bdbb442a335dcc509d
Vyacheslav <101479422+Juryxa@users.noreply.github.com>
2023-09-11 22:43:25 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+const startDate = new Date ("Sep 12, 2022 00:00:00").getTime();
+
+let x = setInterval(function (){
+  let now = new Date().getTime();
+  let distance = now - startDate;
+  let years = Math.floor(distance / (1000 * 60 * 60 * 24 * 365))
+  let days = Math.floor(distance % (1000 * 60 * 60 * 24 * 365) / (1000 * 60 * 60 * 24)).toString().padStart(2,"0");
+  let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)).toString().padStart(2,"0");
+  let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60)).toString().padStart(2,"0");
+  let seconds = Math.floor((distance % (1000 * 60)) / 1000).toString().padStart(2,"0");
+  document.getElementById("time").innerHTML = years + " : " + days + " : " + hours + " : " + minutes + " : " + seconds;
+},1000)
+
