edf2a683fe3328dd6c04d2c92b35d0e797021250
Vyacheslav <101479422+Juryxa@users.noreply.github.com>
2023-11-16 21:30:35 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ let x = setInterval(function (){
   let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)).toString().padStart(2,"0");
   let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60)).toString().padStart(2,"0");
   let seconds = Math.floor((distance % (1000 * 60)) / 1000).toString().padStart(2,"0");
-  document.getElementById("time").innerHTML = years + " : " + days + " : " + hours + " : " + minutes + " : " + seconds;
+  document.getElementsByClassName("time")[0].innerHTML = years + " : " + days + " : " + hours + " : " + minutes + " : " + seconds;
+  document.getElementsByClassName('time2')[0].innerHTML = Math.floor(distance / (1000 * 60 * 60 * 24)).toString().padStart(2, "0") + ' d.';
 },1000)
 
